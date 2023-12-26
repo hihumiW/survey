18281f6be63f38d513ea259c307503f01f186ae0
yamamoto <qq931266421@yahoo.co.jp>
2023-12-26 10:39:05 +0800

    up

--- src/util/fetchBaseQuery.js
+++ src/util/fetchBaseQuery.js
@@ -3,11 +3,16 @@ const fetchBaseQuery = () => (url, config) => {
   if (!headers.has("Content-Type")) {
     headers.append("Content-Type", "application/json");
   }
+  const Authorization = localStorage.getItem("gcp-portal-token");
+  if (Authorization) {
+    headers.append("Authorization", Authorization);
+  }
   const fetchUrl = import.meta.env.VITE_REQUEST_BASE_URL + url;
 
   return fetch(fetchUrl, {
     headers,
     ...restConfig,
+    credentials: "omit",
   }).then((resp) =>
     resp.json().then((resp) => {
       if (resp.success || resp.code === 200) {
